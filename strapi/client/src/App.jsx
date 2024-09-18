9a2d9570ffc4e312fdd95e390762b70d91551e8e
bollytn <ps3online85@gmail.com>
2024-09-18 03:08:10 +0100

    add client and server file project

---
+++ strapi/client/src/App.jsx
@@ -0,0 +1,15 @@
+import Products from './components/Products'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Products></Products>
+    </>
+  )
+}
+
+export default App
---
+++ strapi/client/src/components/Products.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import useFetch from "../Hooks/useFetch"
+
+export default function products() {
+    const [products, setProducts] = useState([])
+    const { data, loading, error } = useFetch("/products?populate=*")
+
+    useEffect(() => {
+        data && setProducts(data)
+        console.log(products);
+    }, [data])
+
+    return (
+        <>
+            {loading
+                ? "loading... "
+                : products.map(product => (
+                    <>
+                        <div key={product.id}>{product.attributes.Title}</div>
+                        <img src={import.meta.env.VITE_APP_URL + product.attributes.image.data.attributes.url} alt={product.title} />
+                    </>
+                ))}
+        </>
+    )
+}
