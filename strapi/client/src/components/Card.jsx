efde338408af0d6998aed49c527c796e34cf5dd0
bollytn <ps3online85@gmail.com>
2024-09-19 23:50:39 +0100

    "Updated Card component with new UI and styles"

--- strapi/client/src/components/Card.jsx
+++ strapi/client/src/components/Card.jsx
@@ -1,7 +1,33 @@
 import './card.css'
+import { CiShoppingBasket } from "react-icons/ci";
 
 export default function Card() {
   return (
-    <div>Card</div>
+    <div className="cart">
+      <div className="cart-icon"><CiShoppingBasket /></div>
+      <div className="cart-badge">5</div>
+      <ul className="cart-list">
+        <li className="cart-item">
+          <img src="http://localhost:1337/uploads/men_02_6574cf6348.jpg" alt="" className="cart-item-image" />
+          <span className="cart-item-title">Classic Sprint</span>
+          <span className="cart-item-price">120$</span>
+        </li>
+        <li className="cart-item">
+          <img src="http://localhost:1337/uploads/men_03_f9a87cf369.jpg" alt="" className="cart-item-image" />
+          <span className="cart-item-title">Classic Sprint</span>
+          <span className="cart-item-price">120$</span>
+        </li>
+        <li className="cart-item">
+          <img src="http://localhost:1337/uploads/women_01_c0b1a2b538.jpg" alt="" className="cart-item-image" />
+          <span className="cart-item-title">Classic Sprint</span>
+          <span className="cart-item-price">120$</span>
+        </li>
+        <li className="cart-item">
+          <img src="http://localhost:1337/uploads/women_02_652a34c8e5.jpg" alt="" className="cart-item-image" />
+          <span className="cart-item-title">Classic Sprint</span>
+          <span className="cart-item-price">120$</span>
+        </li>
+      </ul>
+    </div>
   )
 }
